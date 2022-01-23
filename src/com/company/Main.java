b7aeed2427a08d5f90281fcdb92816b6b6a4e06d
dev <dev@rabins-iMac.local>
2022-01-23 14:39:03 -0800

    java file created

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int a = 12;
+	int b = 8;
+	int sum = a + b;
+        System.out.println(sum);
+    }
+}
