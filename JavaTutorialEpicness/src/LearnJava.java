03fba039f9a5951a6d59ef49591bd8fcc0b6dc15
dev <dev@rabins-iMac.local>
2022-01-23 15:32:58 -0800

    created java folder

---
+++ JavaTutorialEpicness/src/LearnJava.java
@@ -0,0 +1,13 @@
+public class LearnJava {
+    public static void main(String[] args) {
+      String name = "Susan";
+      String lastName = "Prajapati";
+        System.out.println(name.toLowerCase());
+        System.out.println(lastName.toUpperCase());
+        addExclamationPoint("hot dogs");
+    }
+
+    public static void addExclamationPoint(String s) {
+        System.out.println(s + "!");
+    }
+}
